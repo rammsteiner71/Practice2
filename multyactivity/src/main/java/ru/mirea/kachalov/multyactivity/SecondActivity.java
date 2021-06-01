256620071bec9dbfa2ccc0179d11e15f5f76e2af
rammsteiner71 <71226653+rammsteiner71@users.noreply.github.com>
2021-06-01 19:22:33 +0300

    Initial commit

---
+++ multyactivity/src/main/java/ru/mirea/kachalov/multyactivity/SecondActivity.java
@@ -0,0 +1,23 @@
+package ru.mirea.kachalov.multyactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        String text = (String) getIntent().getSerializableExtra("key");
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(text);
+    }
+}
